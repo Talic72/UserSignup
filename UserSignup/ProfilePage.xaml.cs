d1c321e145ddea6a02a8d9869fd4c3357e5665ac
Talic72 <EMORRISWORK72@GMAIL.COM>
2026-06-28 15:49:59 -0400

    Completed MAUI signup app.

--- UserSignup/ProfilePage.xaml.cs
+++ UserSignup/ProfilePage.xaml.cs
@@ -1,9 +1,34 @@
+using Microsoft.Maui.Controls;
+
 namespace UserSignup;
 
+[QueryProperty(nameof(Username), "username")]
+[QueryProperty(nameof(Email), "email")]
 public partial class ProfilePage : ContentPage
 {
-	public ProfilePage()
-	{
-		InitializeComponent();
-	}
+    public string Username
+    {
+        set
+        {
+            UsernameLabel.Text = value;
+        }
+    }
+
+    public string Email
+    {
+        set
+        {
+            EmailLabel.Text = value;
+        }
+    }
+
+    public ProfilePage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnSignOutClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//MainPage");
+    }
 }
